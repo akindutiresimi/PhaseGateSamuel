12dd04c973ad4af90bc31661f02babe4407429e0
Akindutire Simi <akindutiresimi@gmail.com>
2026-08-19 17:05:55 +0100

    PhaseGateSamuel

---
+++ day Three/java/afterbreak/StringCounting1.java
@@ -0,0 +1,27 @@
+public class StringCounting1{
+
+    public static char word(char[] alphabet){
+
+    for (int count = 0; count < alphabet.length(); count++){
+
+        alphabet. CharAt[count] = alphabet;            
+
+        if(count == 'A' && count == 'B' && count == 'c' && count == 'd' && count == 'E' && count == 'F'){
+
+            alphabet = null;
+                    
+            
+        }
+    return alphabet; 
+    } 
+
+    }
+
+    
+    public static void main(String[] args){
+
+    char[] alphabet = {"abecdef"};
+
+    System.out.println(Word(alphabet));
+    }
+} 
---
+++ day Three/java/afterbreak/StringCounting2.java
@@ -0,0 +1,29 @@
+public class StringCounting2{
+
+    public static char word(char[] alphabet){
+
+
+    for (int count = 0; count < alphabet.length(); count++){
+
+        for(int counter = 0; counter <= alphabet(count); counter++){
+
+               alphabet.CharAt[count] = alphabet.CharAt[counter];
+               
+                return alphabet;
+            
+        }
+
+    } 
+
+    }
+
+
+    public static void main(String[] args){
+
+    char[] alphabet = {"a111BAcb"};
+
+    System.out.println(Word(alphabet));
+    }
+
+
+}
