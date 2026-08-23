08053d1cd4579402d2813e69f5fe07257774a5a6
Akindutire Simi <akindutiresimi@gmail.com>
2026-08-23 23:38:17 +0100

    PhaseGateSamuel

---
+++ day six /java/Mbti.java
@@ -0,0 +1,37 @@
+public class Mbti{
+
+    public static string[] personalityTest(String[] words){
+
+    String[] word1 = {"expend energy, enjoy groups", "conserve energy, enjoy one-on-one"};
+    String[] word2 = {"Interpret literally", "look for meaning and possibilities"};
+    String[] word3 = {"logical, thinking, questioning", "empathetic, feeling, accommodating"};
+    String[] word4 = {"organized, orderly", "flexible, adaptable"};
+    String[] word5 = {"more outgoing, think out loud", "more reserved, think to yourself"};
+    String[] word6 = {"practical, realistic, experiential", "imaginative, innovative, theoretical"};
+    String[] word7 = {"candid, straight forward, frank", "tactful, kind, encouraging"};
+    String[] word8 = {"plan, schedule", "unplanned, spontaneous"};
+    String[] word9 = {"seek many tasks, public activities, interaction with others", "seek private, solitary activities with quiet to concentrate"};
+    String[] word10 = {"standard, usual, conventional", "different, novel, unique"};
+    String[] word11 = {"firm, tend to criticize, hold the line", "gentle, tend to appreciate, conciliate"};
+    String[] word12 = {"regulated, structured", "easy-going, live and let live"};
+    String[] word13 = {"external, communicative, express yourself", "internal, reticent, keep to yourself"};
+    String[] word14 = {"focus on here-and-now", "look to the future, global perspective, big picture"};
+    String[] word15 = {"tough-minded, just", "tender-hearted, merciful"};
+    String[] word16 = {"preparation, plan ahead", "go with the flow, adapt as you go"};
+    String[] word17 = {"active, initiate", "reflective, deliberate"};
+    String[] word18 = {"facts, things, what is", "ideas, dreams, what could be, philosophical"};
+    String[] word19 = {"matter of fact, issue-oriented", "sensitive, people-oriented, compassionate"};
+    String[] word20 = {"control, govern", "latitude, freedom"};
+Extroverted E Vs Introverted I: Questions 1, 5, 9, 13, 17
+Sensing S Vs Intuitive N: Questions 2, 6, 10, 14, 18
+Thinking T Vs Feeling F: Questions 3, 7, 11, 15, 19
+Judging J Vs Perceptive P: Questions 4, 8, 12, 16, 20
+
+semicolοn
+
+(c) Property of Semicolon Village Limited. All rights reserved by the company.
+
+
+    }
+
+]
